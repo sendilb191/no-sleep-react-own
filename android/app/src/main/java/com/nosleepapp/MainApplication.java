24f5752dd6cf215c2046f131b7aa76083cb5b8e3
Sendil B <sendilb191@gmail.com>
2025-12-16 15:23:25 +0530

    Fix: Implement DeviceLock native module for Android

--- android/app/src/main/java/com/nosleepapp/MainApplication.java
+++ android/app/src/main/java/com/nosleepapp/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
+import com.nosleepapp.DeviceLockPackage; // Import the DeviceLock package
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -21,7 +22,8 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           return Arrays.<ReactPackage>asList(
-              new MainReactPackage()
+              new MainReactPackage(),
+              new DeviceLockPackage() // Register the DeviceLock package
           );
         }
 
